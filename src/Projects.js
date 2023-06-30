d3830598221224f2ff27f818de49f78c73a53aa7
Daniel Mironiuk <softwarebydanielmironiuk@gmail.com>
2023-06-29 20:12:01 -0400

    Got video cloudfront distribution working

--- src/Projects.js
+++ src/Projects.js
@@ -1,35 +1,29 @@
 import MatrixRainAnimation from './Animation';
-import {Container} from 'react-bootstrap';
 import './css/Projects.css';
 
 function Projects() {
-    return (
-        <>
-            <MatrixRainAnimation className="matrix" />
-            <div className='gps-proj text-center'>
-                <h1 className='text white projects-title'>Projects</h1>
-                <div className='gps-proj d-flex align-items-center'>
-                    <GPS_PROJECT/>
-                </div>
-            </div>
-            <MatrixRainAnimation className="matrix" />
-            <div className='gps-proj text-center'>
-                <div className='gps-proj d-flex align-items-center'>
-                    <GPS_PROJECT/>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <MatrixRainAnimation className="matrix" />
+      <div className='text-center'>
+        <h1 className='text white projects-title'>Projects</h1>
+        <div className='d-flex justify-content-center'>
+          <GPS_PROJECT />
+        </div>
+      </div>
+    </>
+  );
 }
 
 function GPS_PROJECT() {
-    return (
-        <Container className='projects-container'>
-            <div className='project-content text-center'>
-                <h3 className='text'>GPS State Capitol Project</h3>
-            </div>
-        </Container>
-    )
+  return (
+    <div className='projects-container'>
+      <h3 className='text'>GPS State Capitol Project</h3>
+      <video width="100%" height="100%" controls>
+        <source src='https://d1a8mx70ybu1ae.cloudfront.net/GPS_State_Capitol_Project.mov' type="video/mp4"/>
+      </video>
+    </div>
+  )
 }
 
 export default Projects;
